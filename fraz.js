9c12f9581ce001652fefb0c4254563689e89d678
Michael <michaelfrazis@gmail.com>
2017-06-25 19:23:46 +1000

    made templates work

--- fraz.js
+++ fraz.js
@@ -2,3 +2,72 @@ var express = require('express')
 
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main'
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/views'));
+
+// set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+app.get('/investorletters', function(req, res) {
+    res.render('investorletters');
+});
+
+app.get('/people', function(req, res) {
+    res.render('people');
+});
+
+app.get('/people', function(req, res) {
+    res.render('views/people');
+});
+
+app.get('/strategy', function(req, res) {
+    res.render('strategy');
+});
+
+app.get('/xero', function(req, res) {
+    res.render('xero');
+});
+
+
+app.get('/investorletters', function(req,res){
+    res.render('investorletters');
+});
+
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.' );
+});
